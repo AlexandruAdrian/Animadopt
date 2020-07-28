0d71c34828221fe3c71c50df29463a5cda0d607b
Alexandru Curca <alexandru.adrian.curca@gmail.com>
2020-07-28 14:01:50 +0300

    The app now sends a confirmation email when a new user registers

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,7 +1,13 @@
 const User = require("../models/userModel");
 const RandomCode = require("../models/randomCodeModel");
+const Mailer = require("../mailer/index");
 const bcrypt = require("bcrypt");
 const { ConflictError } = require("../errors/index");
+require("dotenv").config();
+
+const HOSTNAME = process.env.HOSTNAME;
+const PORT = process.env.PORT;
+const API_V = process.env.API_V;
 
 class UserController {
   async userRegister(userData) {
@@ -30,10 +36,16 @@ class UserController {
     delete newUser.password;
 
     // Create a random code and save it
-    const confirmationCode = new RandomCode({
+    const activationCode = new RandomCode({
       forUserId: newUser._id,
     });
-    confirmationCode.save();
+    activationCode.save();
+
+    // Send email for account activation with the generated code
+    const activationLink = `https://${HOSTNAME}:${PORT}/api/${API_V}/users?confirmation=${activationCode._id}`;
+    const subject = "Activare cont";
+    const text = `Apăsați pe următorul link pentru a activa contul ${activationLink}`;
+    const url = await Mailer.send(newUser.email, subject, text);
 
     return newUser;
   }
