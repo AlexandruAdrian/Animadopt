4f1fc12bd67e2149c16ac28fea7cb7f69cd9dcdd
Alexandru Curca <alexandru.adrian.curca@gmail.com>
2020-08-07 15:58:13 +0300

    Added route and controller for creating a new post

--- api/controllers/postController.js
+++ api/controllers/postController.js
@@ -1,5 +1,20 @@
 const Post = require("../models/postModel");
 
-class PostController {}
+class PostController {
+  async createPost(pictures, postData, userId) {
+    const newPost = new Post({
+      postedBy: userId,
+      title: postData.title,
+      description: postData.description,
+      type: postData.type,
+      location: postData.location,
+      pictures: pictures,
+    });
+
+    await newPost.save();
+
+    return newPost;
+  }
+}
 
 module.exports = new PostController();
--- api/routes/postRoutes.js
+++ api/routes/postRoutes.js
@@ -1,16 +1,40 @@
 const express = require("express");
 const PostController = require("../controllers/postController");
+const upload = require("../middlewares/multer");
 const isAuthorized = require("../middlewares/authorization");
 
 const postRoutes = () => {
   const router = express.Router();
 
-  router.post("/create", isAuthorized, async (req, res, next) => {
-    try {
-    } catch (err) {
-      next(err);
+  router.post(
+    "/",
+    isAuthorized,
+    upload.array("pictures", 5),
+    async (req, res, next) => {
+      try {
+        const picturesPath = [];
+        const userId = req.user._id;
+        const postData = req.body;
+
+        req.files.forEach((picture) => {
+          picturesPath.push(picture.path);
+        });
+
+        const newPost = await PostController.createPost(
+          picturesPath,
+          postData,
+          userId
+        );
+
+        res.status(201).json({
+          message: "Postarea a fost creata cu succes",
+          post: newPost,
+        });
+      } catch (err) {
+        next(err);
+      }
     }
-  });
+  );
 
   return router;
 };
